f54a9a65fb756fae49b55734f6b36c54254c5b2e
samarkun <samarkun4@gmail.com>
2026-01-29 16:54:50 +0530

    tanstack and viem both use there

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { createPublicClient, http } from 'viem'
 import './App.css'
 import { mainnet } from 'viem/chains'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 
-const client = createPublicClient({
-  chain: mainnet,
-  transport: http(),
-})
 
+async function getBalanc() {
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  })
+  const res = await client.getBalance({ address: "0x7D5f294529F53a7aF4e71EBbec9ccF1f1052D1a6" })
+
+  return res.toString();
+}
+
+const queryClient = new QueryClient();
 function App() {
-
-  async function getBalance() {
-    const res = await client.getBalance({address: "0x7D5f294529F53a7aF4e71EBbec9ccF1f1052D1a6"})
-    console.log(res)
-  }
-
   return (
-    <div>
-      <button onClick={getBalance}>Get Balance</button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Todos />
+    </QueryClientProvider>
   )
 
+}
+
+function Todos() {
+  const query = useQuery({ queryKey: ['res'], queryFn: getBalanc, refetchInterval: 10 * 1000 })
+
+  return <div>
+    Balance: {query.data}
+  </div>
 }
 
 
